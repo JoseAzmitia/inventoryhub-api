b9083c38c6a60b23c7c9bac0149b1dedb6199e8e
JoseAzmitia <azmitiajc@gmail.com>
2023-04-10 16:18:56 -0600

    Se crea el proyecto

---
+++ src/main/java/com/azmitia/inventoryhub100/InventoryhubApplication.java
@@ -0,0 +1,13 @@
+package com.azmitia.inventoryhub100;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryhubApplication.class, args);
+	}
+
+}
